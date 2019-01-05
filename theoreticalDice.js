fb2c2907dcf840c7c9615041ff7050d927dfab4e
Brent Hazman <bdhazman@gmail.com>
2019-01-05 01:31:58 -0500

    initial commit

---
+++ theoreticalDice.js
@@ -0,0 +1,34 @@
+var diceRolls = [];
+
+function populateDiceRolls(){
+  for(i = 1; i <= 6; i++){
+    for(j = 1; j<= 6; j++){
+      diceRolls.push(i+j);
+    }
+  }
+}
+
+function shuffleDiceRolls(){
+  for(i = 0; i < 36; i++){
+    var k = Math.floor(Math.random() * i+1);
+    var temp = diceRolls[k];
+    diceRolls[k] = diceRolls[i];
+    diceRolls[i] = temp;
+  }
+}
+
+function rollDice(){
+  var number = diceRolls.pop();
+  document.getElementById("number").innerHTML = number;
+  if(diceRolls.length == 0){
+    populateDiceRolls();
+    shuffleDiceRolls();
+    console.log(diceRolls);
+  }
+}
+
+populateDiceRolls();
+console.log(diceRolls);
+
+shuffleDiceRolls();
+console.log(diceRolls);
